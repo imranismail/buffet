2f5e11d0d58e192c574df984ce378b86607232b5
Imran Ismail <imran.codely@gmail.com>
2018-10-20 16:19:59 +0800

    WIP

--- lib/buffet.ex
+++ lib/buffet.ex
@@ -1,9 +1,7 @@
 defmodule Buffet do
   defmacro define(protobuf) do
     with {:ok, parse_tree, _context, _, _, _} <- Buffet.Parser.parse(protobuf) do
-      parse_tree
-      |> IO.inspect()
-      |> Buffet.Compiler.compile()
+      Buffet.Compiler.compile(parse_tree)
     else
       {:error, reason, _context, _, _, _} ->
         raise RuntimeError, message: reason
